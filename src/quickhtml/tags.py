185a0e7fabe00a50599b16eac68cdf051e1a0206
Rashid N H M <95639609+rashidnhm@users.noreply.github.com>
2024-08-05 23:11:31 -0400

    Format

--- src/quickhtml/tags.py
+++ src/quickhtml/tags.py
@@ -55,7 +55,7 @@ class BaseTag:
             callback (typing.Callable): The callback function to be added.
         """
         self.callback_route = f"/python-callbacks/{id(callback)}"
-        
+
         self.app.add_route(
             self.callback_route,
             callback,
