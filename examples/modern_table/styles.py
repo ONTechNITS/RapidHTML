09688aac69c2058556b3f80ec06e7828c15df658
Rashid N H M <95639609+rashidnhm@users.noreply.github.com>
2024-08-08 20:20:32 -0400

    Remove usage of duplicate key in styling

--- examples/modern_table/styles.py
+++ examples/modern_table/styles.py
@@ -7,8 +7,6 @@ stylesheet = {
         "font-size": "0.9em",
         "min-width": "400px",
         "box-shadow": "0 0 20px rgba(0, 0, 0, 0.15)",
-    },
-    ".styled-table": {
         "thead": {
             "tr": {
                 "background-color": "#009879",
@@ -23,7 +21,7 @@ stylesheet = {
             "tr:nth-of-type(even)": {"background-color": "#f3f3f3"},
             "tr:last-of-type": {"border-bottom": "2px solid #009879"},
         },
-    },
+    }
 }
 
 table_styling = StyleSheet(**stylesheet)
