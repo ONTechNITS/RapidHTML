d95bdec7039d96dda1402d6c25b8248b5fca3828
Thomas Ryan <thomryan@cisco.com>
2024-08-16 11:20:03 -0400

    Added docs to Renderable

--- src/rapidhtml/bases.py
+++ src/rapidhtml/bases.py
@@ -2,5 +2,13 @@ import abc
 
 
 class Renderable:
+    """
+    Base renderable class
+
+    Defines anything that should be rendered when placed into a
+    RapidHTMLResponse. Any class that inherits from this should define it's own
+    `render` method. The ooutput of this method should be a string that can be
+    used in a modern web browser.
+    """
     @abc.abstractmethod
-    def render(self): ...
+    def render(self) -> str: ...
