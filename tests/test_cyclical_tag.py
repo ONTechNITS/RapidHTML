9e3027e3c97b632c660438828293a831468ecf37
Rashid N H M <95639609+rashidnhm@users.noreply.github.com>
2024-10-25 00:14:27 -0400

    Added a very deep cyclical tag check

--- tests/test_cyclical_tag.py
+++ tests/test_cyclical_tag.py
@@ -1,3 +1,5 @@
+import random
+
 import pytest
 
 import rapidhtml.exceptions
@@ -22,3 +24,20 @@ class TestCyclicalTag:
         tag2 = Div()
         tag1.add_tag(tag2)
         assert True
+
+    def test_deep_cyclical_tag(self):
+        
+        NUM_TAGS = 1_000
+
+        root_tag = Div()
+        tag_list = [Div() for _ in range(NUM_TAGS)]
+        
+        # Add all tags to the root tag
+        root_tag.add_tag(*tag_list)
+        
+        # Add all tags that come AFTER the current iteration of tag
+        sample_tag_index = random.randint(0, NUM_TAGS - 1)
+        tag_list[sample_tag_index].add_tag(*tag_list[sample_tag_index + 1:])
+
+        with pytest.raises(rapidhtml.exceptions.CyclicalTagError):
+            tag_list[-1].add_tag(root_tag)
